28c4e8676f6ca670072266c2bade98fd1aa05f4e
Herbert Turner <hturner@andover.edu>
2017-07-28 14:37:25 -0500

    Unity Project

---
+++ Assets/Buttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+	public void Tutorial ()
+	{
+		SceneManager.LoadScene ("Tutorial");
+	}
+
+	public void Explore ()
+	{
+
+		SceneManager.LoadScene ("Menu");
+	}
+}
---
+++ Assets/Click.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Click : MonoBehaviour
+{
+
+	public void TaskOnClick()
+	{
+		
+		Debug.Log("You have clicked the button!");
+		SceneManager.LoadScene("Tutorial");
+	}
+}
